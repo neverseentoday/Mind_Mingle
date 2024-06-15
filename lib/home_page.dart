7bb052cdab6318a9072bcded61834e994452aee3
Rahul Boney <rahulboney14@gmail.com>
2024-06-15 21:39:57 +0530

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:mind_mingle/Comps/text_field.dart';
+import 'package:mind_mingle/WebDevCourses.dart';
 import 'package:mind_mingle/job_page.dart';
+import 'package:mind_mingle/splash_screen.dart';
+import 'package:mind_mingle/login_page.dart';
 import 'package:mind_mingle/Comps/side_menu.dart';
 import 'package:mind_mingle/teachPage.dart';
 import 'package:mind_mingle/Comps/list.dart';
 import 'package:mind_mingle/GetHiredPage.dart';
+import 'package:mind_mingle/BasicCourses.dart';
+import 'package:mind_mingle/Comps/aboutus.dart';
+import 'package:mind_mingle/Comps/contact us.dart';
+import 'package:mind_mingle/Comps/tc.dart';
+import 'package:mind_mingle/SoftwareCourses.dart';
+import 'package:mind_mingle/bdcourses.dart';
+import 'package:mind_mingle/Comps/userProfile.dart';
+import 'package:mind_mingle/pdcourse.dart';
 class home_page extends StatelessWidget {
   final searchController = TextEditingController();
-
-  home_page({super.key});
   @override
   Widget build(BuildContext context) {
     var arrNames=['Software Development Courses','Basic Courses','Business Courses','Web Development Courses'];
@@ -22,52 +32,49 @@ class home_page extends StatelessWidget {
                 padding: const EdgeInsets.only(top: 52),
                 child: Container(
                   //color: Colors.blue,
-                  child: Row(
-                    //mainAxisAlignment: MainAxisAlignment.start,
-                    //crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextButton(onPressed:(){
-                          Scaffold.of(context).openDrawer();
-                        },
-                          child: const Padding(
-                            padding: EdgeInsets.only(left: 4.0),
-                            child: Icon(Icons.account_circle,size: 48,color: Colors.black,),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      //mainAxisAlignment: MainAxisAlignment.start,
+                      //crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextButton(onPressed:(){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>MyAppe()));
+                          },
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 4.0),
+                              child: Icon(Icons.account_circle,size: 48,color: Colors.black,),
+                            ),
                           ),
-                        ),
-                        Container(width: 42,),
-                        Image.asset('assets/images/app_logo.png',height: 52,width: 100,),
-                        Container(width: 42,),
-                        const Padding(
-                          padding: EdgeInsets.only(top: 10.0,right:18.0),
-                          child: Icon(Icons.add_box_outlined,size: 35,),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.only(right: 5.0,top: 10.0),
-                          child: Icon(Icons.shopping_cart_outlined,size: 35,),
-                        ),
-                      ]
+                          Container(width: 42,),
+                          Image.asset('assets/images/app_logo.png',height: 52,width: 100,),
+                          Container(width: 42,),
+                          Container(margin:EdgeInsets.only(left: 10.0),child: IconButton(onPressed:(){}, icon:Icon(Icons.favorite_border),iconSize:36 ,)),
+                          IconButton(onPressed: (){}, icon:Icon(Icons.shopping_cart_outlined),iconSize: 36,)
+                        ]
+                    ),
                   ),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(9.0),
-                child: SizedBox(
+                child: Container(
                   width: 300,
                   height: 40,
                   child: TextField(
                     decoration: InputDecoration(
                         focusedBorder:OutlineInputBorder(
                             borderRadius: BorderRadius.circular(25.0),
-                            borderSide: const BorderSide(
+                            borderSide: BorderSide(
                               color: Colors.black,
                               width: 2,
                             )
                         ),
                         hintText: 'Search for courses',
-                        prefixIcon:const Icon(Icons.search),
+                        prefixIcon:Icon(Icons.search),
                         enabledBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(25.0),
-                            borderSide:const BorderSide(
+                            borderSide:BorderSide(
                                 color:Colors.grey,
                                 width:2
                             )
@@ -85,9 +92,9 @@ class home_page extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.only(left:16.0),
                     child: OutlinedButton(
-                        child:const Text('Post a job',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontFamily: 'AppFont'),),
+                        child:Text('Post a job',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontFamily: 'AppFont'),),
                         onPressed:(){
-                          Navigator.pushReplacement(context,MaterialPageRoute(builder: (context)=>const job_page()));
+                          Navigator.pushReplacement(context,MaterialPageRoute(builder: (context)=>job_page()));
                         }
                     ),
                   ),
@@ -95,8 +102,8 @@ class home_page extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.only(right: 1),
                     child: OutlinedButton(onPressed: (){
-                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>const teach_page()));
-                    }, child: const Text('Teach on MindMingle',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontFamily: 'AppFont'))),
+                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>teach_page()));
+                    }, child: Text('Teach on MindMingle',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontFamily: 'AppFont'))),
                   ),
                 ],
 
@@ -105,12 +112,12 @@ class home_page extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(right:54.0),
                 child: OutlinedButton(onPressed: (){
-                  Navigator.push(context,MaterialPageRoute(builder: (context)=>const GetHired()));
-                }, child:const Text('Get Hired',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontFamily: 'AppFont'))),
+                  Navigator.push(context,MaterialPageRoute(builder: (context)=>GetHired()));
+                }, child:Text('Get Hired',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontFamily: 'AppFont'))),
               ),
-              
-              const Padding(
-                  padding: EdgeInsets.only(right: 60.0,top: 20.0),
+
+              Padding(
+                  padding: const EdgeInsets.only(right: 60.0,top: 20.0),
                   child: Center(
                       child: Text('Courses',
                         style:TextStyle(fontWeight: FontWeight.bold,fontSize: 18 ,
@@ -119,55 +126,65 @@ class home_page extends StatelessWidget {
                       )
                   )
               ),
-              const SizedBox(height: 20,),
+              SizedBox(height: 20,),
               Column(
                 children: [
                   Row(
                     children: [
-                      const Padding(
-                        padding: EdgeInsets.only(left: 25.0),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 25.0),
                         child: Text('Basic Courses ',style: TextStyle(fontFamily: 'AppFont',fontWeight: FontWeight.bold,fontSize: 17),),
                       ),
-                      TextButton(onPressed:(){}, child:const Icon(Icons.arrow_forward_sharp) ),
+                      TextButton(onPressed:(){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>MyApp()));
+                      }, child:Icon(Icons.arrow_forward_sharp) ),
                     ],
                   ),
                   Row(
                     children: [
-                      const Padding(
-                        padding: EdgeInsets.only(left: 25.0),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 25.0),
                         child: Text('Software Development Courses ',style: TextStyle(fontFamily: 'AppFont',fontWeight: FontWeight.bold,fontSize: 17),),
                       ),
-                      TextButton(onPressed:(){}, child:const Icon(Icons.arrow_forward_sharp) ),
+                      TextButton(onPressed:(){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>Myapp()));
+                      }, child:Icon(Icons.arrow_forward_sharp) ),
                     ],
                   ),
                   Row(
                     children: [
-                      const Padding(
-                        padding: EdgeInsets.only(left: 25.0),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 25.0),
                         child: Text('Web Development Courses ',style: TextStyle(fontFamily: 'AppFont',fontWeight: FontWeight.bold,fontSize: 17),),
                       ),
-                      TextButton(onPressed:(){}, child:const Icon(Icons.arrow_forward_sharp) ),
+                      TextButton(onPressed:(){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>MyAppp()));
+                      }, child:Icon(Icons.arrow_forward_sharp) ),
                     ],
                   ),
                   Row(
                     children: [
-                      const Padding(
-                        padding: EdgeInsets.only(left: 25.0),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 25.0),
                         child: Text('Business Development Courses ',style: TextStyle(fontFamily: 'AppFont',fontWeight: FontWeight.bold,fontSize: 17),),
                       ),
-                      TextButton(onPressed:(){}, child:const Icon(Icons.arrow_forward_sharp) ),
+                      TextButton(onPressed:(){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>MyApppp()));
+                      }, child:Icon(Icons.arrow_forward_sharp) ),
                     ],
                   ),
                   Row(
                     children: [
-                      const Padding(
-                        padding: EdgeInsets.only(left: 25.0),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 25.0),
                         child: Text('Personal Development Courses ',style: TextStyle(fontFamily: 'AppFont',fontWeight: FontWeight.bold,fontSize: 17),),
                       ),
-                      TextButton(onPressed:(){}, child:const Icon(Icons.arrow_forward_sharp) ),
+                      TextButton(onPressed:(){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>MyAppppp()));
+                      }, child:Icon(Icons.arrow_forward_sharp) ),
                     ],
                   ),
-                  const SizedBox(width:50 ,height: 10,),
+                  Container(width:50 ,height: 10,),
                 ],
               ),
               Column(
@@ -180,14 +197,20 @@ class home_page extends StatelessWidget {
                       children: [
                         Container(
                             alignment: Alignment.center,
-                            child: TextButton(onPressed:(){}, child:const Text('About Us',style: TextStyle(color: Colors.white),) )),
+                            child: TextButton(onPressed:(){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=>AboutUsPage()));
+                            }, child:Text('About Us',style: TextStyle(color: Colors.white),) )),
 
                         Container(
                             alignment: Alignment.center,
-                            child: TextButton(onPressed: (){}, child: const Text('Contact Us',style: TextStyle(color: Colors.white),))),
+                            child: TextButton(onPressed: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=>ContactUsPage()));
+                            }, child: Text('Contact Us',style: TextStyle(color: Colors.white),))),
                         Container(
                             alignment: AlignmentDirectional.center,
-                            child: TextButton(onPressed: (){}, child: const Text('Terms and Conditions',style: TextStyle(color: Colors.white)))),
+                            child: TextButton(onPressed: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=>TermsAndConditionsPage()));
+                            }, child: Text('Terms and Conditions',style: TextStyle(color: Colors.white)))),
 
                       ],
                     ),
